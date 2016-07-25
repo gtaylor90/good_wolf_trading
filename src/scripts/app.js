d8482fc92f2912d7f8f56b1d6ecafa3acead625b
gtaylor90 <graham.taylor.90@gmail.com>
2016-07-25 13:45:44 -0500

    some stuff

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,11 +5,26 @@ import init from './init'
 
 
 const app = function() {
-  document.querySelector('.container').innerHTML = "<h1>Woah!</h1>"
+  Router = Backbone.Router.extend({
+
+    routes: {
+      "*": "main"
+      // routes
+    },
+
+    initialize: function (args) {
+      // super
+    },
+
+    main: function () {
+      // main
+    }
+
+  });
 }
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
-// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
+// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE.
 export const app_name = init()
 app()
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
