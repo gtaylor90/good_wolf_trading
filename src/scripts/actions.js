fc7e4c7011bef90a995d3192ce87b3dd91ac8417
gtaylor90 <graham.taylor.90@gmail.com>
2016-07-27 13:28:51 -0500

    actions shit

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -13,6 +13,32 @@ const ACTIONS = {
     }).then(function() {
       STORE.emitChange()
     })
+  },
+  registerUser: function(userObj){
+    User.register(userObj).then(()=>this.logUserIn(userObj.email,
+      userObj.password),
+      (err)=>{
+        alert('FAILURE to register')
+        console.log(err)
+      }
+    )
+  },
+  logUserIn: function(email, password){
+    User.login(email, password).then(
+      (resp)=> {
+        alert(`user ${email} logged in!`)
+        console.log(resp)
+        location.hash= "home"
+      },
+      (err)=>{
+        alert('FAILURE logging in')
+        console.log(err)
+      })
+  },
+  logUserOut: function(){
+    User.logout().then(
+      ()=> location.hash="login"
+    )
   }
 }
 
