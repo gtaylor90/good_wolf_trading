7c81de2d4650732a7f9daf2aaa0950feeb26d878
gtaylor90 <graham.taylor.90@gmail.com>
2016-07-28 14:39:31 -0500

    api router

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,11 +4,11 @@ let helpers = require('../config/helpers.js')
 
 let User = require('../db/schema.js').User
 
-  
+
   apiRouter
     .get('/users', function(req, res){
       User.find(req.query , "-password", function(err, results){
-        if(err) return res.json(err) 
+        if(err) return res.json(err)
         res.json(results)
       })
     })
@@ -16,7 +16,7 @@ let User = require('../db/schema.js').User
   apiRouter
     .get('/users/:_id', function(req, res){
       User.findById(req.params._id, "-password", function(err, record){
-        if(err || !record ) return res.json(err) 
+        if(err || !record ) return res.json(err)
         res.json(record)
       })
     })
@@ -25,7 +25,7 @@ let User = require('../db/schema.js').User
         if(err || !record) return res.json(err)
         let recordWithUpdates = helpers.updateFields(record, req.body)
         recordWithUpdates.save(function(err){
-          if(err) return res.json(err) 
+          if(err) return res.json(err)
           res.json(recordWithUpdates)
         })
       })
@@ -37,10 +37,20 @@ let User = require('../db/schema.js').User
           msg: `record ${req.params._id} successfully deleted`,
           _id: req.params._id
         })
-      })  
+      })
     })
 
     // Routes for a Model(resource) should have this structure
-
+    apiRouter.post('/cards', function(request, response){
+         let dish = new Card(request.body) //create new instance of schema
+       //request.body is all the information we gathered from the client side
+         card.save(function(err){ //handling errors
+            if(err){
+              response.send(err)
+            } else {
+              response.json(dish)
+            }
+         })
+      })
 
 module.exports = apiRouter
