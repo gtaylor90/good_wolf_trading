249b16e39d7fe6f8416c5cc8c878b216c1aed615
gtaylor90 <graham.taylor.90@gmail.com>
2016-08-05 15:56:54 -0500

    more user binder stuff

--- src/scripts/views/userbinders.js
+++ src/scripts/views/userbinders.js
@@ -5,9 +5,42 @@ import ACTIONS from '../actions'
 import $ from 'jquery'
 import { User } from '../models/models'
 
+const UserBinder = React.createClass({
+  componentWillMount() {
+    if(!User.getCurrentUser()){
+      location.hash="login"
+    }
+    else {
+      ACTIONS.fetchBinder({
+        cardOwner: User.getCurrentUser()._id
+      })
+      STORE.on('updateContent', ()=>{
+        this.setState(STORE.getData())
+      })
+    }
+
+  },
+  getInitialState() {
+    return STORE.getData()
+  },
+  render(){
+    return(
+      <div>
+        <BinderRender uID={this.props.uID} />
+      </div>
+    )
+  }
+})
 
 const UserBinderView = React.createClass({
-  
+  render(){
+    return(
+      <div>
+        <Header />
+        <BinderRender uID={this.props.uID} cardColl={this.state} />
+      </div>
+    )
+  }
 })
 
 export default UserBinderView
